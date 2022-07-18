9eccad8fc725b8887403a9efbe51d22f0d8def04
Julianahlk <julianayasnog@gmail.com>
2022-07-18 14:13:46 +0300

    Feat: Setup state variable for close delay

--- src/modules/users/components/GasStation/GasStationPopover.tsx
+++ src/modules/users/components/GasStation/GasStationPopover.tsx
@@ -28,6 +28,7 @@ const GasStationPopover = ({
   transactionAndMessageGroups,
 }: Props) => {
   const [isOpen, setOpen] = useState(false);
+  const [useCloseDelay, setUseCloseDelay] = useState(false);
   const [txNeedsSigning, setTxNeedsSigning] = useState(false);
   const txCount = useMemo(() => transactionCount(transactionAndMessageGroups), [
     transactionAndMessageGroups,
@@ -38,8 +39,9 @@ const GasStationPopover = ({
     if (prevTxCount != null && txCount > prevTxCount) {
       setOpen(true);
       setTxNeedsSigning(true);
+      setUseCloseDelay(true);
     }
-  }, [txCount, prevTxCount, setTxNeedsSigning]);
+  }, [txCount, prevTxCount, setTxNeedsSigning, useCloseDelay]);
 
   /*
    * @NOTE Offset Calculations
@@ -74,10 +76,11 @@ const GasStationPopover = ({
       placement="bottom"
       showArrow={false}
       isOpen={isOpen}
-      closeDelay={4000}
+      closeAfterDelay={useCloseDelay}
       onClose={() => {
         setOpen(false);
         setTxNeedsSigning(false);
+        setUseCloseDelay(false);
       }}
       popperOptions={{
         modifiers: [
